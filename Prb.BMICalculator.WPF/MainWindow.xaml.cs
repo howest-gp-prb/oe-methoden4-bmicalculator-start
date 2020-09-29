67751600dcdd75e8eeee61296d6736062806addb
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-29 09:06:42 +0200

    Namespace adjusted

--- Prb.BMICalculator.WPF/MainWindow.xaml.cs
+++ Prb.BMICalculator.WPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Methoden4.BMICalculator.WPF
+namespace Prb.BMICalculator.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
